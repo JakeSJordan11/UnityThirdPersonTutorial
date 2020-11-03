2b43eb6b90c22f08880c73136c9e8eba9d8be371
jakesjordan11 <jakesjordan11@gmail.com>
2020-11-03 11:40:55 -0700

    added basic camera and movement

---
+++ Assets/Scripts/CharacterControllerLogic.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControllerLogic : MonoBehaviour
+{
+    #region Variables (private)
+
+    [SerializeField]
+    private Animator animator;
+    [SerializeField]
+    private float directionDampTime = .25f;
+
+    private float speed = 0.0f;
+    private float h = 0.0f;
+    private float v = 0.0f;
+
+    #endregion
+
+    #region Unity event functions
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        if (animator.layerCount >= 2)
+        {
+            animator.SetLayerWeight(1, 1);
+        }
+    }
+
+    void Update()
+    {
+        if (animator)
+        {
+            // pull values from controller/keyboard
+            h = Input.GetAxis("Horizontal");
+            v = Input.GetAxis("Vertical");
+
+            speed = new Vector2(h, v).sqrMagnitude;
+
+            animator.SetFloat("Speed", speed);
+            animator.SetFloat("Direction", h, directionDampTime, Time.deltaTime);
+
+            // Debug.Log("speed = " + "[" + speed + "] " + "horizontal = " + "[" + h + "]");
+        }
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    #region Variables (private)
+
+    [SerializeField]
+    private float distanceAway = 0;
+    [SerializeField]
+    private float distanceUp = 0;
+    [SerializeField]
+    private float smooth = 0;
+    [SerializeField]
+    private Transform follow;
+    private Vector3 targetPosition;
+
+    #endregion
+
+    #region Unity event functions
+
+    void Start()
+    {
+        follow = GameObject.FindWithTag("Player").transform;
+    }
+
+    void LateUpdate()
+    {
+        // setting the target position to be the correct offset from the target
+        targetPosition = follow.position + follow.up * distanceUp - follow.forward * distanceAway;
+        Debug.DrawRay(follow.position, Vector3.up * distanceUp, Color.red);
+        Debug.DrawRay(follow.position, -1f * follow.forward * distanceAway, Color.blue);
+        Debug.DrawLine(follow.position, targetPosition, Color.magenta);
+
+        // making a smooth transition between it's current position and the position it wants to be in
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smooth);
+
+        // make sure the camera is looking the right way
+        transform.LookAt(follow);
+    }
+
+    #endregion
+}
